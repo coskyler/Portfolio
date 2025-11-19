27b865bf7eb4578a620e379c1ea249efd3c9e254
Skyler Quinby <skylerpquinby@gmail.com>
2025-11-19 00:13:11 -0500

    Fix background effect stuck on screen

--- src/components/threads.js
+++ src/components/threads.js
@@ -37,6 +37,8 @@ export default function Threads() {
     const draw = () => {
       scrollPos = window.scrollY * 0.5;
       if(scrollPos > canvas.height) {
+        ctx.fillStyle = "rgba(0,0,0,.3)";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
         frame = requestAnimationFrame(draw);
         return;
       }
